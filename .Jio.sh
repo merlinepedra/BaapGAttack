7e04e978ec3210b49a7c1feb2af0bd31d034f249
BaapG <87407116+Vretlee@users.noreply.github.com>
2021-09-27 16:58:58 +0530

    Create .Jio.sh
    B

---
+++ .Jio.sh
@@ -0,0 +1,2 @@
+defps1="PS1='\\\[\\\e[39m\\\]\\\[\\\e[32m\\\]# \\\[\\\e[0m\\\]\\\[\\\e[39m\\\]𝐁𝐚𝐚𝐩𝐆 @яσσт : - \\\[\\\e[0m\\\]\\\[\\\e[0m\\\]\\\[\\\e[36m\\\]'"
+sed -i "s/.*PS1=.*/$defps1/" /data/data/com.termux/files/usr/etc/bash.bashrc
